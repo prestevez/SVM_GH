aac4287d3cd58b98fd6b701762cf3feeee04297c
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-04-16 19:43:01 +0100

    Added implementation of svm_test

--- 03_Analysis_Results.R
+++ 03_Analysis_Results.R
@@ -1,2 +1,10 @@
 # 03_Analysis_Results.R
 
+sink(logfile, append=TRUE, type=c("output", "message"))
+
+svm_search <- svm_test(data=flowdata, m=c(1:3), col=c(1:3), W=W1, ii=TRUE, sig=NULL,
+                       C=10, epsilon=0.1, cross=5, trainp=(181*154), outWD=dirRoutput,
+                       mainWD=mainWD)
+
+# Basic svm_search. Parameters accept several values, except W.
+# The sig defaults to NULL for automatic sigma estimation.
