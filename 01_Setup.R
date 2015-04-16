eec6ad3e176f4917db764e0f0c3de95c4977db2d
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-04-16 18:54:42 +0100

    Added the packages section

---
+++ 01_Setup.R
@@ -0,0 +1,13 @@
+## Begin 01_Setup.R
+
+# # Install packages
+# install.packages("kernlab")
+# install.packages("ggplot2")
+# install.packages("knitr")
+
+# Load packages to library
+
+library(kernlab)
+library(ggplot2)
+library(knitr)
+
