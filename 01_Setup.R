73a9ffaabe4c461ddf84307f6ab918e0bffab9bf
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-04-16 18:59:42 +0100

    Added dir objects

--- 01_Setup.R
+++ 01_Setup.R
@@ -11,3 +11,23 @@ library(kernlab)
 library(ggplot2)
 library(knitr)
 
+# Set up objects for WD paths
+
+# Main WD
+mainWD <- "~/R Workspace/STDM Coursework/SVM_GH"
+
+# Stores Raw Data
+dirRawData <- "~/R Workspace/STDM Coursework/SVM_GH/RawData"
+
+# Directory for the R data
+dirRdata <- "~/R Workspace/STDM Coursework/SVM_GH/Rdata"
+
+# Output directories
+# for graphical output 
+dirRoutput <- "~/R Workspace/STDM Coursework/SVM_GH/GraphicOutput"
+
+# for text
+dirText <- "~/R Workspace/STDM Coursework/SVM_GH/TextOutput"
+
+
+### END of 01_Setup.R
