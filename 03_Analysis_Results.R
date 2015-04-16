c7d16733f35a51b9bd81ccf83857dee4e1f72a7b
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-04-16 19:45:27 +0100

    Added results section and end of file line

--- 03_Analysis_Results.R
+++ 03_Analysis_Results.R
@@ -8,3 +8,13 @@ svm_search <- svm_test(data=flowdata, m=c(1:3), col=c(1:3), W=W1, ii=TRUE, sig=N
 
 # Basic svm_search. Parameters accept several values, except W.
 # The sig defaults to NULL for automatic sigma estimation.
+
+## Results
+
+svm_search
+
+# markdown table of results
+
+kable(svm_search$svm_results, format="markdown")
+
+# End of 03_Analysis_Results.R
