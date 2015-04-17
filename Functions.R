f5b4c1df16f8ab7c3f2ec2476c48cbe886277f56
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-04-17 15:16:47 +0100

    Restored older version

--- Functions.R
+++ Functions.R
@@ -224,7 +224,7 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
 svm_test <- function(data=NULL, m=NULL, col=NULL, W=NULL, sig=NULL,
                      ii=TRUE, C=10, epsilon=0.1, cross=5,
                      trainp=NULL, outWD=NULL, mainWD=NULL, 
-                     nrmse=FALSE, graphs=FALSE, interval=NULL)
+                     nrmse=FALSE, graphs=FALSE)
 {
   # Inputs:
   # data: Matrix for the SVM
@@ -240,7 +240,6 @@ svm_test <- function(data=NULL, m=NULL, col=NULL, W=NULL, sig=NULL,
   # mainWD: name of main WD
   # nrmse: Logical, if TRUE RMSE is calculated using NRMSE (standardised)
   # graphs: Logical, if TRUE graphs will be produced
-  # interval: The time interval (spacing) of the series, defaults to NULL
   
   # Initialise variables for best error and best model
   best_model <- NULL
@@ -255,6 +254,7 @@ svm_test <- function(data=NULL, m=NULL, col=NULL, W=NULL, sig=NULL,
   
   colnames(svm_results)[7:9] <- c("Training Error" ,"Resiudal Error",
                                   "Predicted Error") 
+  
   
   # If NRMSE is TRUE
   
@@ -280,25 +280,133 @@ svm_test <- function(data=NULL, m=NULL, col=NULL, W=NULL, sig=NULL,
       {
         for (k in 1:length(epsilon))
         {
-          for (c in 1:length(interval))
+          if (is.null(sig))
           {
-            if (is.null(sig))
+            kp <- "automatic"
+            
+            # Embed the time series
+            st_data <- st_embed(data=data, m=m[a], col=col[b], W=W, ii=ii)
+            
+            # Separate the embedded time series in training and validation data
+            Xtr <- st_data$X[1:trainp]
+            ytr <- st_data$y[1:trainp]
+            Xts <- st_data$X[(trainp+1):nrow(st_data$X)]
+            yts <- st_data$y[(trainp+1):nrow(st_data$y)]
+            
+            model <- ksvm(x=Xtr, y=ytr, type="eps-svr", kernel="rbfdot",
+                          kpar=kp, C=C[j], epsilon=epsilon[k],
+                          cross=cross)
+
+            # Create objects to include in results Table
+            tr_error <- error(model)
+            tr_pred <- predict(model, Xtr)
+            tr_rmse <- rmse(tr_pred, ytr)
+            ts_pred <- predict(model, Xts)
+            ts_rmse <- rmse(yts, ts_pred)
+            sig_val <- kpar(kernelf(model))$sigma
+            link_name <- colnames(data)[col[b]]
+            minutes <- (m[a])*5
+            
+            # Add data to results table
+            svm_results[ind,1] <- ind
+            svm_results[ind,2] <- minutes
+            svm_results[ind,3] <- link_name
+            svm_results[ind,4] <- sig_val
+            svm_results[ind,5] <- C[j]
+            svm_results[ind,6] <- epsilon[k]
+            svm_results[ind,7] <- tr_error
+            svm_results[ind,8] <- tr_rmse
+            svm_results[ind,9] <- ts_rmse
+            
+            
+            if (graphs==TRUE)
             {
-              kp <- "automatic"
+              # Create and save graphs
+              obs_values_tr <- data.frame(ytr, "Observed", 1:length(ytr))
+              colnames(obs_values_tr) <- c("value", "class", "time")
+              pred_values_tr <- data.frame(tr_pred, "Predicted", 1:length(tr_pred))
+              colnames(pred_values_tr) <- c("value", "class", "time")
+              
+              trainval_df <- rbind(obs_values_tr, pred_values_tr)
+              trainval_df_one <- subset(trainval_df, time %in% c(1:181))
+              
+              obs_values_ts <- data.frame(yts, "Observed", 1:length(yts))
+              colnames(obs_values_ts) <- c("value", "class", "time")
+              pred_values_ts <- data.frame(ts_pred, "Predicted", 1:length(ts_pred))
+              colnames(pred_values_ts) <- c("value", "class", "time")
+              
+              testval_df <- rbind(obs_values_ts, pred_values_ts)
+              testval_df_one <- subset(testval_df, time %in% c(1:181))
+              
+              p1 <- ggplot(data=trainval_df, aes(x=time, y=value, colour=class, 
+                                                 linetype=class))+
+                geom_line(size=0.4) +
+                scale_color_manual(values=c("Observed"="black", "Predicted"="red")) +
+                ggtitle(paste(minutes, " mins. ", link_name,". " ,"Training set: Model ", 
+                              ind, sep="")) +
+                xlab("") + ylab("Seconds per metre") +
+                theme(legend.title=element_blank())
+              
+              p2 <- ggplot(data=trainval_df_one,
+                           aes(x=time, y=value, colour=class, linetype=class))+
+                geom_line(size=0.4) +
+                scale_color_manual(values=c("Observed"="black", "Predicted"="red")) +
+                ggtitle(paste(minutes, " mins. ", link_name,". " 
+                              ,"One Day Training set: Model ",ind, sep="")) +
+                xlab("") + ylab("Seconds per metre") +
+                theme(legend.title=element_blank())
+              
+              p3 <- ggplot(data=testval_df, aes(x=time, y=value, colour=class, 
+                                                linetype=class))+
+                geom_line(size=0.4) +
+                scale_color_manual(values=c("Observed"="black", "Predicted"="red")) +
+                ggtitle(paste(minutes, " mins. ", link_name,". " ,"Testing set: Model ",
+                              ind, sep="")) +
+                xlab("") + ylab("Seconds per metre") +
+                theme(legend.title=element_blank())
+              
+              p4 <- ggplot(data=testval_df_one,
+                           aes(x=time, y=value, colour=class, linetype=class))+
+                geom_line(size=0.4) +
+                scale_color_manual(values=c("Observed"="black", "Predicted"="red")) +
+                ggtitle(paste(minutes, " mins. ", link_name,". " ,
+                              "One Day Testing set: Model ", ind, sep="")) +
+                xlab("") + ylab("Seconds per metre") +
+                theme(legend.title=element_blank())
+              
+              file <- paste(minutes, "mins", "_", link_name, "_", "Model", 
+                            ind, ".pdf", sep="")
+              
+              setwd(outWD)
+              pdf(file, width=11.7, height=8.3)
+              multiplot(p1, p2, p3, p4, cols=2)
+              dev.off()
+              setwd(mainWD)
+            }
+            
+            else
+            {
+              
+            }
+            
+            
+            if(tr_error < best_error)
+            {
+              best_error <- tr_error
+              best_model <- model
+              best_time <- paste(minutes, "minutes", sep=" ")
+            }
+            ind <- ind + 1
+          }
+          
+          else
+          {
+            for (i in 1:length(sig))
+            {
+              kp <- list(sigma=sig[i])
               
               # Embed the time series
-              if (is.null(interval))
-              {
-                st_data <- st_embed(data=data, m=m[a], col=col[b], W=W, ii=ii)
-              }
-              
-              else
-              {
-                st_data <- st_embed(data=data, m=m[a], col=col[b], W=W, ii=ii, 
-                                    interval=interval[c])
-                
-                svm_results[ind,10] <- interval[c]
-              }
+              st_data <- st_embed(data=data, m=m[a], col=col[b], W=W, ii=ii)
               
               # Separate the embedded time series in training and validation data
               Xtr <- st_data$X[1:trainp]
@@ -330,8 +438,6 @@ svm_test <- function(data=NULL, m=NULL, col=NULL, W=NULL, sig=NULL,
               svm_results[ind,7] <- tr_error
               svm_results[ind,8] <- tr_rmse
               svm_results[ind,9] <- ts_rmse
-              svm_results[ind,10] <- NA
-              
               
               if (graphs==TRUE)
               {
@@ -412,140 +518,8 @@ svm_test <- function(data=NULL, m=NULL, col=NULL, W=NULL, sig=NULL,
               }
               ind <- ind + 1
             }
-            
-            else
-            {
-              for (i in 1:length(sig))
-              {
-                kp <- list(sigma=sig[i])
-                
-                # Embed the time series
-                if (is.null(interval))
-                {
-                  st_data <- st_embed(data=data, m=m[a], col=col[b], W=W, ii=ii)
-                }
-                
-                else
-                {
-                  st_data <- st_embed(data=data, m=m[a], col=col[b], W=W, ii=ii, 
-                                      interval=interval[c])
-                  
-                  svm_results[ind,10] <- interval[c]
-                }
-                
-                # Separate the embedded time series in training and validation data
-                Xtr <- st_data$X[1:trainp]
-                ytr <- st_data$y[1:trainp]
-                Xts <- st_data$X[(trainp+1):nrow(st_data$X)]
-                yts <- st_data$y[(trainp+1):nrow(st_data$y)]
-                
-                model <- ksvm(x=Xtr, y=ytr, type="eps-svr", kernel="rbfdot",
-                              kpar=kp, C=C[j], epsilon=epsilon[k],
-                              cross=cross)
-                
-                # Create objects to include in results Table
-                tr_error <- error(model)
-                tr_pred <- predict(model, Xtr)
-                tr_rmse <- rmse(tr_pred, ytr)
-                ts_pred <- predict(model, Xts)
-                ts_rmse <- rmse(yts, ts_pred)
-                sig_val <- kpar(kernelf(model))$sigma
-                link_name <- colnames(data)[col[b]]
-                minutes <- (m[a])*5
-                
-                # Add data to results table
-                svm_results[ind,1] <- ind
-                svm_results[ind,2] <- minutes
-                svm_results[ind,3] <- link_name
-                svm_results[ind,4] <- sig_val
-                svm_results[ind,5] <- C[j]
-                svm_results[ind,6] <- epsilon[k]
-                svm_results[ind,7] <- tr_error
-                svm_results[ind,8] <- tr_rmse
-                svm_results[ind,9] <- ts_rmse
-                svm_results[ind,10] <- NA
-                
-                if (graphs==TRUE)
-                {
-                  # Create and save graphs
-                  obs_values_tr <- data.frame(ytr, "Observed", 1:length(ytr))
-                  colnames(obs_values_tr) <- c("value", "class", "time")
-                  pred_values_tr <- data.frame(tr_pred, "Predicted", 1:length(tr_pred))
-                  colnames(pred_values_tr) <- c("value", "class", "time")
-                  
-                  trainval_df <- rbind(obs_values_tr, pred_values_tr)
-                  trainval_df_one <- subset(trainval_df, time %in% c(1:181))
-                  
-                  obs_values_ts <- data.frame(yts, "Observed", 1:length(yts))
-                  colnames(obs_values_ts) <- c("value", "class", "time")
-                  pred_values_ts <- data.frame(ts_pred, "Predicted", 1:length(ts_pred))
-                  colnames(pred_values_ts) <- c("value", "class", "time")
-                  
-                  testval_df <- rbind(obs_values_ts, pred_values_ts)
-                  testval_df_one <- subset(testval_df, time %in% c(1:181))
-                  
-                  p1 <- ggplot(data=trainval_df, aes(x=time, y=value, colour=class, 
-                                                     linetype=class))+
-                    geom_line(size=0.4) +
-                    scale_color_manual(values=c("Observed"="black", "Predicted"="red")) +
-                    ggtitle(paste(minutes, " mins. ", link_name,". " ,"Training set: Model ", 
-                                  ind, sep="")) +
-                    xlab("") + ylab("Seconds per metre") +
-                    theme(legend.title=element_blank())
-                  
-                  p2 <- ggplot(data=trainval_df_one,
-                               aes(x=time, y=value, colour=class, linetype=class))+
-                    geom_line(size=0.4) +
-                    scale_color_manual(values=c("Observed"="black", "Predicted"="red")) +
-                    ggtitle(paste(minutes, " mins. ", link_name,". " 
-                                  ,"One Day Training set: Model ",ind, sep="")) +
-                    xlab("") + ylab("Seconds per metre") +
-                    theme(legend.title=element_blank())
-                  
-                  p3 <- ggplot(data=testval_df, aes(x=time, y=value, colour=class, 
-                                                    linetype=class))+
-                    geom_line(size=0.4) +
-                    scale_color_manual(values=c("Observed"="black", "Predicted"="red")) +
-                    ggtitle(paste(minutes, " mins. ", link_name,". " ,"Testing set: Model ",
-                                  ind, sep="")) +
-                    xlab("") + ylab("Seconds per metre") +
-                    theme(legend.title=element_blank())
-                  
-                  p4 <- ggplot(data=testval_df_one,
-                               aes(x=time, y=value, colour=class, linetype=class))+
-                    geom_line(size=0.4) +
-                    scale_color_manual(values=c("Observed"="black", "Predicted"="red")) +
-                    ggtitle(paste(minutes, " mins. ", link_name,". " ,
-                                  "One Day Testing set: Model ", ind, sep="")) +
-                    xlab("") + ylab("Seconds per metre") +
-                    theme(legend.title=element_blank())
-                  
-                  file <- paste(minutes, "mins", "_", link_name, "_", "Model", 
-                                ind, ".pdf", sep="")
-                  
-                  setwd(outWD)
-                  pdf(file, width=11.7, height=8.3)
-                  multiplot(p1, p2, p3, p4, cols=2)
-                  dev.off()
-                  setwd(mainWD)
-                }
-                
-                else
-                {
-                  
-                }
-                
-                
-                if(tr_error < best_error)
-                {
-                  best_error <- tr_error
-                  best_model <- model
-                  best_time <- paste(minutes, "minutes", sep=" ")
-                }
-                ind <- ind + 1
-              }
-            }  
           }
+          #           ind <- ind+1
         }
       }
     } 
