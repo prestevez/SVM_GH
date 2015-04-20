6783ea123a055d7946646b9b0f8d8e541e0814de
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-04-20 20:22:11 +0100

    Fixed Typo

--- Functions.R
+++ Functions.R
@@ -252,7 +252,7 @@ svm_test <- function(data=NULL, m=NULL, col=NULL, W=NULL, sig=NULL,
                             "C"=0, "Epsilon"=0, "Train Error"=0, "Residual Error"=0,
                             "Predicted Error"=0)
   
-  colnames(svm_results)[7:9] <- c("Training Error" ,"Resiudal Error",
+  colnames(svm_results)[7:9] <- c("Training Error" ,"Residual Error",
                                   "Predicted Error") 
   
   
@@ -263,7 +263,7 @@ svm_test <- function(data=NULL, m=NULL, col=NULL, W=NULL, sig=NULL,
     rmse <- nrmse
     
     # Modify data frame to store results
-    colnames(svm_results)[8:9] <- c("Standard Resiudal Error", "Standard Predicted Error") 
+    colnames(svm_results)[8:9] <- c("Standard Residual Error", "Standard Predicted Error") 
   }
   else
   {
