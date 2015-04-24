79fa37bd80585434eef8bad86efcd3078c395617
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-04-24 11:30:31 +0100

    So far, only m, no interval option

--- no_loop_funs.R
+++ no_loop_funs.R
@@ -1,10 +1,12 @@
 ## Using lapply to embedd the time series to many links, add m options, add interval options.
 
-m <- c(1:2)
-interval <- c(1,6)
-col <- c(1:2)
+m <- c(1)
+interval <- c(1)
+col <- c(1:3)
 W <- W1
 data <- flowdata
+day_len <- 181
+train_d <- 12
 
 # Multiple spatio_temporal embedding function
 m_embed <- function(m, interval, col, W, data)
@@ -30,28 +32,67 @@ system.time(data_small <- m_embed(data=flowdata, m=m, col=col, interval=interval
 
 # Separate the embedded time series in training and validation data
 
-day_len <- 181
-train_d <- 12
-m <- c(1)
-interval <- c(1)
-
+# Generates the length of the training periods
+# working fun
 # Generates the length of the training periods
 trainp <- function(x, y, data, m, interval)
 {
-  unlist(lapply(1:length(m), function(mm, x, y, data, m, interval)
+  lapply(1:length(m), function(mm, x, y, data, m, interval)
+  {
+    lapply(1:length(data), function(dd, x, y, data, m, interval)
     {
-      lapply(1:length(data), function(dd, x, y, data, m, interval)
+      nrow(data[[dd]]) - interval*m - x*y
+    },
+    x=x, y=y, data=data, interval=interval, m=m[mm])
+  }, 
+  x=x, y=y, data=data, interval=interval, m=m)
+}
+
+
+# work in progress
+trainp <- function(x, y, data, m, interval)
+{
+  lapply(1:length(m), function(mm, x, y, data, m, interval)
+    {
+      lapply(1:length(interval), function(int, x, y, data, m, interval)
         {
-          nrow(data[[dd]]) - interval*m - x*y
+          lapply(1:length(data), function(dd, x, y, data, m, interval)
+            {
+              nrow(data[[dd]]) - interval*m - x*y
+            }, 
+            x=x, y=y, data=data, interval=interval[int], m=m)
         },
         x=x, y=y, data=data, interval=interval, m=m[mm])
-    }, x=x, y=y, data=data, interval=interval, m=m))
+    }, 
+    x=x, y=y, data=data, interval=interval, m=m)
 }
 
 
 period <- trainp(x=day_len, y=train_d, data=data_small, interval=interval, m=m)
 
 # Splits up into training and testing sets
+# working fun
+splitdata <- function(x, y, data, m, interval)
+{
+  # Create training period
+  period <- trainp(x=x, y=y, data=data, m=m, interval=interval)
+  period <- unlist(period)
+  # Training sets
+  Training <- lapply(1:length(period), function(dd, x, data)
+  {
+    data[[dd]][1:x[dd],]
+  }, data=data, x=period)
+  
+  # Testing sets
+  Testing <- lapply(1:length(period), function(dd, x, data)
+  {
+    data[[dd]][(x[dd]+1):nrow(data[[dd]]),]
+  }, data=data, x=period)
+  return(list(Training=Training, Testing=Testing))
+}
+
+
+## work in progress
 splitdata <- function(x, y, data, m, interval)
 {
   # Create training period
@@ -71,7 +112,7 @@ splitdata <- function(x, y, data, m, interval)
   return(list(Training=Training, Testing=Testing))
 }
 
-tr_sets <- splitdata(x=day_len, y=train_d, data=data_small, interval=interval, m=m)
+system.time(tr_sets <- splitdata(x=day_len, y=train_d, data=data_small, interval=interval, m=m))
 
 
 
