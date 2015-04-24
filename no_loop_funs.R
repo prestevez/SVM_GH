9f2e7bdaa60132928cacc839d62ce160398f5178
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-04-24 11:45:48 +0100

    Added multicore to funs

--- no_loop_funs.R
+++ no_loop_funs.R
@@ -1,14 +1,14 @@
 ## Using lapply to embedd the time series to many links, add m options, add interval options.
 
-m <- c(1)
+m <- c(1:2)
 interval <- c(1)
-col <- c(1:3)
+col <- c(1:2)
 W <- W1
 data <- flowdata
 day_len <- 181
 train_d <- 12
 
-# Multiple spatio_temporal embedding function
+# Multiple spatio_temporal embedding function, one core version
 m_embed <- function(m, interval, col, W, data)
 {
   unlist(lapply(1:length(col), function(z, m, interval, W, data, col)
@@ -26,6 +26,23 @@ m_embed <- function(m, interval, col, W, data)
   m=m, interval=interval, col=col, W=W, data=data), recursive=FALSE)
 }
 
+# Multiple spatio_temporal embedding function, mclapply version, about twice as fast.
+m_embed <- function(m, interval, col, W, data)
+{
+  unlist(mclapply(1:length(col), function(z, m, interval, W, data, col)
+  {
+    unlist(lapply(1:length(interval), function(int, m, interval, W, data, col)
+    {
+      unlist(lapply(1:length(m), function(mm, m, interval, W, data, col)
+      {
+        st_embed(data=data, m=m[mm], col=col, W=W, ii=TRUE, interval=interval)
+      },
+      m=m, interval=interval[int], col=col, W=W, data=data), recursive=FALSE)
+    },
+    m=m, interval=interval, col=col[z], W=W, data=data), recursive=FALSE)
+  },
+  m=m, interval=interval, col=col, W=W, data=data, mc.cores=detectCores()), recursive=FALSE)
+}
 
 system.time(data_small <- m_embed(data=flowdata, m=m, col=col, interval=interval, W=W))
 
@@ -37,7 +54,7 @@ system.time(data_small <- m_embed(data=flowdata, m=m, col=col, interval=interval
 # Generates the length of the training periods
 trainp <- function(x, y, data, m, interval)
 {
-  lapply(1:length(m), function(mm, x, y, data, m, interval)
+  mclapply(1:length(m), function(mm, x, y, data, m, interval)
   {
     lapply(1:length(data), function(dd, x, y, data, m, interval)
     {
@@ -45,7 +62,7 @@ trainp <- function(x, y, data, m, interval)
     },
     x=x, y=y, data=data, interval=interval, m=m[mm])
   }, 
-  x=x, y=y, data=data, interval=interval, m=m)
+  x=x, y=y, data=data, interval=interval, m=m, mc.cores=defaultCores())
 }
 
 
