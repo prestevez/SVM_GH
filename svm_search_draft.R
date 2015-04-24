7cca982c9f96d7b67427466c3b20632a123b0a31
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-04-24 11:31:03 +0100

    mclapply for genrating many ksvm models from the data

---
+++ svm_search_draft.R
@@ -0,0 +1,28 @@
+
+
+tr_sets[[1]]
+
+ul <- unlist(tr_sets, recursive=FALSE)
+
+system.time(models <- lapply(seq(1,length(ul)/4, 1), function(dd, data)
+{
+  Xtr <- data[[dd]]
+  ytr <- data[[dd+1]]
+  ksvm(x=Xtr, y=ytr, type="eps-svr", kernel="rbfdot",
+                 kpar="automatic", epsilon=0.1, cross=5)
+},
+data=ul)
+)
+  
+# mclapply option
+require(parallel)
+
+system.time(models <- mclapply(seq(1,length(ul)/4, 1), function(dd, data)
+{
+  Xtr <- data[[dd]]
+  ytr <- data[[dd+1]]
+  ksvm(x=Xtr, y=ytr, type="eps-svr", kernel="rbfdot",
+       kpar="automatic", epsilon=0.1, cross=5)
+},
+data=ul, mc.cores=detectCores())
+)
