622357c99a5ee1de4b8a1fa3619b45ded0c4d483
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-04-27 23:19:51 +0100

    first script for searches

--- svm_search1.R
+++ svm_search1.R
@@ -181,7 +181,6 @@ system.time(
   errorlist2 <- modelerrors(models=models2, data=spdata1)
 )
 
-
 # Integrate results in a table
 
 results1 <- tabresults(errorlist1, period1)
@@ -261,11 +260,119 @@ ep <- 0.5
 
 save.image()
 
-
 # Second stage
-m <- c(1)
-interval <- c(1) # can embedd more than one interval at a time
-col <- c(1)
+# third search
+m <- c(1,2,6,12,24)
 
+data2 <- m_embed(data=flowdata, m=m, col=col, interval=interval, W=W)
 
+period2 <- trainp(x=day_len, y=train_d, data=data2, interval=interval, m=m)
+
+spdata2 <- splitdata(x=day_len, y=train_d, data=data2, interval=interval, m=m)
+
+save.image()
+
+# svm_search function with mcore for m*int
+svm_search_mint <- function(data, period, sigma=NULL, C=1, epsilon=0.1)
+{
+  # Inputs:
+  # data: Previously prepared data using m_embed and splitdata functions
+  # period: Length of training periods created with trainp function
+  # sigma: sigma value for svm, defaults to NULL for automatic estimation using sigest
+  # C: Penalisation constant, defaults to 1
+  # epsilon: width of epsilon tube, dedaults to 0.1
+  
+  if (is.null(sigma))
+  {
+    sigma <- 1
+  }
+  names <- names(data)
+  # Start mclapply per link
+  list <- lapply(1:length(data), function(dd, data, period, sigma, C, epsilon)
+  {
+    odd <- seq(1, length(period), 2)
+    even <- seq(2, length(period), 2)
+    pnames <- sub("X_", "", names(period))[c(odd)]
+    # lapply by m*int combinations (reflected in half "period")
+    list2 <- mclapply(1:(length(period)/2),
+                    function(p, data, period, odd, even, sigma, C, epsilon)
+                    {
+                      Xtr <- data[[1]][[odd[p]]]
+                      ytr <- data[[1]][[even[p]]]
+                      if (sigma==1)
+                      {
+                        snames <- "Sigma: Auto"
+                        kp <- "automatic"
+                      }
+                      else
+                      {
+                        snames <- paste("sigma: ", as.character(sigma), sep="")
+                      }
+                      # mclapply by sigma
+                      list3 <- lapply(1:length(sigma),
+                                      function(s, sigma, Xtr, ytr, C, epsilon, kp)
+                                      {
+                                        if (sigma!=1)
+                                        {
+                                          kp <- list(sigma=sigma[s])
+                                        }
+                                        cnames <- paste("C: ", as.character(C), sep="")
+                                        list4 <- lapply(1:length(C), function(c, Xtr, ytr, kp, C, epsilon)
+                                        {
+                                          enames <- paste("epsilon: ", as.character(epsilon), sep="")
+                                          list5 <- lapply(1:length(epsilon),
+                                                          function(e, Xtr, ytr, kp, C, epsilon)
+                                                          {
+                                                            models <- ksvm(x=Xtr, y=ytr, type="eps-svr", kernel="rbfdot",
+                                                                           kpar=kp, C=C, epsilon=epsilon[e], cross=5)
+                                                          },Xtr=Xtr, ytr=ytr, C=C[c], epsilon=epsilon, kp=kp)
+                                          setNames(list5, enames)
+                                        }, Xtr=Xtr, ytr=ytr, kp=kp, C=C, epsilon=epsilon)
+                                        setNames(list4, cnames)
+                                      }, Xtr=Xtr, ytr=ytr, sigma=sigma, C=C, epsilon=epsilon, kp=kp)
+                      setNames(list3, snames)
+                    }, data=data[[dd]], period=period, sigma=sigma, C=C, epsilon=epsilon,
+                    odd=odd, even=even, mc.cores=detectCores())
+    setNames(list2, pnames)
+  }, data=data, period=period, sigma=sigma, C=C, epsilon=epsilon)
+  setNames(list, names)
+}
+
+system.time(
+  models3 <- svm_search_mint(data=spdata2, period=period2, sigma=sigma, C=C, epsilon=ep)
+  )
+
+save.image()
+
+# errors from third search
+system.time(
+  errorlist3 <- modelerrors(models=models3, data=spdata2)
+)
+
+results3 <- tabresults(errorlist3, period2)
+
+#### fourth search
+# m <- c(1)
+interval <- (1,181, 181*7)
+
+data3 <- m_embed(data=flowdata, m=m, col=col, interval=interval, W=W)
+
+period3 <- trainp(x=day_len, y=train_d, data=data3, interval=interval, m=m)
+
+spdata3 <- splitdata(x=day_len, y=train_d, data=data3, interval=interval, m=m)
+
+save.image()
+
+system.time(
+  models3 <- svm_search_mint(data=spdata2, period=period2, sigma=sigma, C=C, epsilon=ep)
+)
+
+save.image()
+
+# errors from third search
+system.time(
+  errorlist3 <- modelerrors(models=models3, data=spdata2)
+)
+
+results3 <- tabresults(errorlist3, period2)
 
